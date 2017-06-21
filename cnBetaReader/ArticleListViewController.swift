95b8d0844688bc1bfff447e424bea374c5bcf058
Shilei Tian <tianshilei1992@gmail.com>
2017-06-21 22:40:06 +0800

    1. Complete the first version of load more function. 2. Rewrite the regex match with string.range(of:) method.

--- cnBetaReader/ArticleListViewController.swift
+++ cnBetaReader/ArticleListViewController.swift
@@ -11,91 +11,113 @@ import Kanna
 import CoreData
 
 class ArticleListViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-  
-  var fetchResultController: NSFetchedResultsController<ArticleMO>!
-  
-  var articlesList: [ArticleMO] = []
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    // Add the refresh control
-    refreshControl = UIRefreshControl()
-    refreshControl?.addTarget(self, action: #selector(ArticleListViewController.getData), for: UIControlEvents.valueChanged)
-    tableView.addSubview(refreshControl!)
+    var fetchResultController: NSFetchedResultsController<ArticleMO>!
+    var articlesList: [ArticleMO] = []
+    let fetchRequest: NSFetchRequest<ArticleMO> = ArticleMO.fetchRequest()
     
-    refreshControl?.beginRefreshing()
-    
-    fetchDataFromLocalStorage()
-  }
-  
-  // MARK: - Init
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-  }
-  
-  // MARK: - Data Source
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return articlesList.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleListCell", for: indexPath) as! ArticleListCell
-    let item = articlesList[indexPath.row]
-    configureDetailsForCell(cell, withArticleListItem: item)
-    return cell
-  }
-  
-  // MARK: - Delegate
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let article = articlesList[indexPath.row]
-    performSegue(withIdentifier: "ShowArticle", sender: article)
-    tableView.deselectRow(at: indexPath, animated: true)
-  }
-  
-  // MARK: - Navigation delegate
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "ShowArticle" {
-      let controller = segue.destination as! ArticleViewController
-      controller.article = sender as! ArticleMO
-    }
-  }
-  
-  // MARK: - Custom Function
-  
-  func configureDetailsForCell(_ cell: ArticleListCell, withArticleListItem item: ArticleMO) {
-    cell.configureForArticleListCell(item)
-  }
-  
-  func fetchDataFromLocalStorage() {
-    let fetchRequest: NSFetchRequest<ArticleMO>! = ArticleMO.fetchRequest()
-    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "time", ascending: false)]
-    if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-      let context = appDelegate.persistentContainer.viewContext
-      fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-      fetchResultController.delegate = self
-      
-      do {
-        try fetchResultController.performFetch()
-        if let fetchedObjects = fetchResultController.fetchedObjects {
-          articlesList = fetchedObjects
-          tableView.reloadData()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Init the fetch result controller
+        initFetchResultController()
+        
+        // Add the refresh control
+        refreshControl = UIRefreshControl()
+        if let refreshControl = refreshControl {
+            refreshControl.addTarget(self, action: #selector(ArticleListViewController.getData), for: UIControlEvents.valueChanged)
+            tableView.addSubview(refreshControl)
+            refreshControl.beginRefreshing()
         }
-      } catch {
-        print(error)
-      }
-      refreshControl?.endRefreshing()
+        
+        fetchDataFromLocalStorage()
     }
-  }
-  
-  func getData() {
-    let httpFetcher = HTTPFetcher()
-    httpFetcher.fetchHomePage(completionHandler: fetchDataFromLocalStorage)
-  }
-  
+    
+    // MARK: - Init
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func initFetchResultController() {
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "time", ascending: false)]
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            let context = appDelegate.persistentContainer.viewContext
+            fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            fetchResultController.delegate = self
+        }
+    }
+    
+    // MARK: - Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articlesList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleListCell", for: indexPath) as! ArticleListCell
+        let item = articlesList[indexPath.row]
+        configureDetailsForCell(cell, withArticleListItem: item)
+        return cell
+    }
+    
+    // MARK: - Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let article = articlesList[indexPath.row]
+        performSegue(withIdentifier: "ShowArticle", sender: article)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Navigation delegate
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowArticle" {
+            let controller = segue.destination as! ArticleViewController
+            controller.article = sender as! ArticleMO
+        }
+    }
+    
+    // MARK: - User defined function
+    
+    func configureDetailsForCell(_ cell: ArticleListCell, withArticleListItem item: ArticleMO) {
+        cell.configureForArticleListCell(item)
+    }
+    
+    func fetchDataFromLocalStorage() {
+        fetchRequest.fetchLimit = 30
+        do {
+            try fetchResultController.performFetch()
+            if let fetchedObjects = fetchResultController.fetchedObjects {
+                articlesList = fetchedObjects
+                tableView.reloadData()
+            }
+        } catch {
+            print(error)
+        }
+        // End freshing if needed
+        if refreshControl != nil && refreshControl!.isRefreshing {
+            refreshControl!.endRefreshing()
+        }
+    }
+    
+    func getData() {
+        let httpFetcher = HTTPFetcher()
+        httpFetcher.fetchHomePage(completionHandler: fetchDataFromLocalStorage, errorHandler: fetchDataError)
+    }
+    
+    // MARK: - Error handler
+    
+    func fetchDataError(errorMessage error: String) {
+        print(error)
+        // End freshing if needed
+        if refreshControl != nil && refreshControl!.isRefreshing {
+            refreshControl!.endRefreshing()
+        }
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
--- cnBetaReader/HTTPFetcher.swift
+++ cnBetaReader/HTTPFetcher.swift
@@ -8,125 +8,178 @@
 
 import Kanna
 
-var loadMoreToken: String? = nil
-
 class HTTPFetcher {
-  
-  // MARK: - APIs
-  
-  // Fetch home page
-  func fetchHomePage(completionHandler: @escaping ()->Void) {
-    let urlString = "http://www.cnbeta.com"
     
-    if let url = URL(string: urlString) {
-      let task = URLSession.shared.dataTask(with: url) {
-        (data, response, error) in
-        if let error = error {
-          print("Error: \(error)")
-        } else if let data = data {
-          if let html = String(data: data, encoding: .utf8), let doc = HTML(html: html, encoding: .utf8) {
-            // Set the load more token
-            let loadMoreTokenElement = doc.at_xpath("//meta[@name='csrf-token']")
-            if let homeMoreTokenElement = loadMoreTokenElement {
-              loadMoreToken = homeMoreTokenElement.content!
-            } else {
-              print("Fatal error: fail to load csrf-token.")
-              return;
+    static var loadMoreToken: String? = nil
+    static var loadMoreParam: String? = nil
+    
+    // MARK: - APIs
+    
+    // Fetch home page
+    func fetchHomePage(completionHandler: @escaping ()->Void, errorHandler: @escaping (_: String)->Void) {
+        let urlString = "http://www.cnbeta.com"
+        
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) {
+                (data, response, error) in
+                if let error = error {
+                    errorHandler("Error: \(error)")
+                } else if let data = data {
+                    if let html = String(data: data, encoding: .utf8), let doc = HTML(html: html, encoding: .utf8) {
+                        // Set the load more token and param
+                        let loadMoreTokenElement = doc.at_xpath("//meta[@name='csrf-token']")
+                        if let homeMoreTokenElement = loadMoreTokenElement {
+                            HTTPFetcher.loadMoreToken = homeMoreTokenElement["content"]!
+                        } else {
+                            errorHandler("Fatal error: fail to parse csrf-token.")
+                            return
+                        }
+                        let loadMoreParamElement = doc.at_xpath("//meta[@name='csrf-param']")
+                        if let loadMoreParamElement = loadMoreParamElement {
+                            HTTPFetcher.loadMoreParam = loadMoreParamElement["content"]!
+                        } else {
+                            errorHandler("Fatal error: fail to parse csrf-param.")
+                            return
+                        }
+                        
+                        // Set the regex for id, time and comment count
+                        var regexTime: NSRegularExpression?, regexCommentCount: NSRegularExpression?
+                        do {
+                            regexTime = try NSRegularExpression(pattern: "\\d\\d-\\d\\d \\d\\d:\\d\\d", options: [])
+                            regexCommentCount = try NSRegularExpression(pattern: "\\d+(?=个意见)", options: [])
+                        } catch {
+                            errorHandler("Error occurred when initializing the regex.")
+                            return
+                        }
+                        
+                        // Process the downloaded item div
+                        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                            for itemDiv in doc.xpath("//div[@class='items-area']/div[@class='item']") {
+                                let article = ArticleMO(context: appDelegate.persistentContainer.viewContext)
+                                let urlElement = itemDiv.at_xpath(".//dl/dt/a")
+                                if let urlElement = urlElement {
+                                    let url = urlElement["href"]!
+                                    article.url = url
+                                    article.title = urlElement.content!
+                                    if let range = url.range(of: "\\d+(?=\\.htm)", options: .regularExpression) {
+                                        article.id = url.substring(with: range)
+                                    } else {
+                                        errorHandler("Fatal error occurred when parsing the article id.")
+                                        return
+                                    }
+                                } else {
+                                    errorHandler("Fatal error occurred when parsing the article url.")
+                                    return
+                                }
+                                
+                                if let statusElement = itemDiv.at_xpath(".//ul[@class='status']/li") {
+                                    let statusString = statusElement.content!
+                                    if let timeMatchResult = regexTime!.firstMatch(in: statusString, options: [], range: NSMakeRange(0, statusString.characters.count)),
+                                        let commentCountMathResult = regexCommentCount!.firstMatch(in: statusString, options: [], range: NSMakeRange(0, statusString.characters.count)) {
+                                        article.time = (statusString as NSString).substring(with: timeMatchResult.range)
+                                        article.commentCount = (statusString as NSString).substring(with: commentCountMathResult.range)
+                                    }
+                                } else {
+                                    errorHandler("Fatal error occurred when parsing the status.")
+                                    return
+                                }
+                                
+                                if let thumbDiv = itemDiv.at_xpath(".//img") {
+                                    article.thumbURL = thumbDiv["src"]!
+                                } else {
+                                    errorHandler("Fatal error occurred when parsing the thumb.")
+                                    return
+                                }
+                                appDelegate.saveContext()
+                            }
+                            completionHandler()
+                        } else {
+                            errorHandler("Failed to get the app delegate.")
+                        }
+                    }
+                }
             }
-            
-            // Set the regex for id, time and comment count
-            var regexID: NSRegularExpression?, regexTime: NSRegularExpression?, regexCommentCount: NSRegularExpression?
-            do {
-              regexID = try NSRegularExpression(pattern: "\\d+", options: [.caseInsensitive])
-              regexTime = try NSRegularExpression(pattern: "\\d\\d-\\d\\d \\d\\d:\\d\\d", options: [.caseInsensitive])
-              regexCommentCount = try NSRegularExpression(pattern: "\\d+(?=个意见)", options: [.caseInsensitive])
-            } catch {
-              regexID = nil
-              regexTime = nil
-              regexCommentCount = nil
-            }
-            
-            // Process the downloaded item div
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-              for itemDiv in doc.xpath("//div[@class='items-area']/div[@class='item']") {
-                let article = ArticleMO(context: appDelegate.persistentContainer.viewContext)
-                let urlElement = itemDiv.at_xpath(".//dl/dt/a")
-                if let urlElement = urlElement {
-                  let url = urlElement["href"]!
-                  article.url = url
-                  article.title = urlElement.content!
-                  if let idMatchResult = regexID?.firstMatch(in: url, options: [], range: NSMakeRange(0, url.characters.count)) {
-                    article.id = (url as NSString).substring(with: idMatchResult.range)
-                  } else {
-                    print("Fatal error: the website structure has changed.")
+            task.resume()
+        }
+    }
+    
+    // Fetch article content
+    func fetchContent(id: String, articleURL: String, completionHandler: @escaping ()->Void) {
+        if let url = URL(string: articleURL) {
+            let task = URLSession.shared.dataTask(with: url) {
+                (data, response, error) in
+                if let error = error {
+                    print("Fatal error: \(error)")
                     return;
-                  }
-                } else {
-                  print("Fatal error: the website structure has changed.")
-                  return;
+                } else if let data = data {
+                    if let html = String(data: data, encoding: .utf8), let doc = HTML(html: html, encoding: .utf8) {
+                        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                            let articleContent = ArticleContentMO(context: appDelegate.persistentContainer.viewContext)
+                            articleContent.id = id
+                            if let summary = doc.at_xpath("//div[@class='article-summary']//p") {
+                                articleContent.summary = String()
+                                articleContent.summary = summary.toHTML!
+                            } else {
+                                print("Failed to parse the summary…")
+                                return
+                            }
+                            articleContent.content = String()
+                            let paras = doc.xpath("//div[@class='article-content']")
+                            for para in paras {
+                                articleContent.content!.append(para.toHTML!)
+                            }
+                            appDelegate.saveContext()
+                            completionHandler()
+                        }
+                    }
                 }
-                
-                if let statusElement = itemDiv.at_xpath(".//ul[@class='status']/li") {
-                  let statusString = statusElement.content!
-                  if let timeMatchResult = regexTime?.firstMatch(in: statusString, options: [], range: NSMakeRange(0, statusString.characters.count)),
-                    let commentCountMathResult = regexCommentCount?.firstMatch(in: statusString, options: [], range: NSMakeRange(0, statusString.characters.count)) {
-                    article.time = (statusString as NSString).substring(with: timeMatchResult.range)
-                    article.commentCount = (statusString as NSString).substring(with: commentCountMathResult.range)
-                  }
-                } else {
-                  print("Fatal error: the website structure has changed.")
-                  return;
-                }
-                
-                if let thumbDiv = itemDiv.at_xpath(".//img") {
-                  article.thumbURL = thumbDiv["src"]!
-                } else {
-                  print("Fatal error: the website structure has changed.")
-                  return;
-                }
-                appDelegate.saveContext()
-              }
-              completionHandler()
             }
-          }
+            task.resume()
         }
-      }
-      task.resume()
     }
-  }
-  
-  // Fetch article content
-  func fetchContent(id: String, articleURL: String, completionHandler: @escaping ()->Void) {
-    if let url = URL(string: articleURL) {
-      let task = URLSession.shared.dataTask(with: url) {
-        (data, response, error) in
-        if let error = error {
-          print("Fatal error: \(error)")
-          return;
-        } else if let data = data {
-          if let html = String(data: data, encoding: .utf8), let doc = HTML(html: html, encoding: .utf8) {
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-              let articleContent = ArticleContentMO(context: appDelegate.persistentContainer.viewContext)
-              articleContent.id = id
-              if let summary = doc.at_xpath("//div[@class='article-summary']//p") {
-                articleContent.summary = String()
-                articleContent.summary = summary.toHTML!
-              } else {
-                print("Failed to parse the summary…")
-                return
-              }
-              articleContent.content = String()
-              let paras = doc.xpath("//div[@class='article-content']")
-              for para in paras {
-                articleContent.content!.append(para.toHTML!)
-              }
-              appDelegate.saveContext()
-              completionHandler()
+    
+    // Load more article
+    func loadMore() {
+        // Set the query items
+        let epochTime = Int(NSDate().timeIntervalSince1970 * 1000)
+        let urlComponents = NSURLComponents(string: "http://www.cnbeta.com/home/more")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: HTTPFetcher.loadMoreParam!, value: HTTPFetcher.loadMoreToken!),
+            URLQueryItem(name: "type", value: "all"),
+            URLQueryItem(name: "page", value: "2"),
+            URLQueryItem(name: "_", value: "\(epochTime)")
+        ]
+        let url = urlComponents?.url
+        if let url = url {
+            // Set the request header, otherwise the app cannot get the right more data.
+            var request = URLRequest(url: url)
+            // Important: The return json will be empty without the referer header.
+            request.setValue("http://www.cnbeta.com/", forHTTPHeaderField: "Referer")
+            let task = URLSession.shared.dataTask(with: request) {
+                (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                } else if let data = data {
+                    do {
+                        let resJSON = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                        let moreArticlesList = (resJSON?["result"] as? [String: Any])?["list"] as? [Any]
+                        if let moreArticlesList = moreArticlesList {
+                            for article in moreArticlesList {
+                                let _article = article as? [String: Any]
+                                let title = _article?["title"] as! String
+                                let articleURL = _article?["url_show"] as! String
+                                let id = _article?["sid"] as! String
+                                let comments = _article?["comments"] as! String
+                                let thumbURL = _article?["thumb"] as! String
+                                print("Title: \(title), url: \(articleURL), sid: \(id), comments: \(comments)")
+                            }
+                        }
+                    } catch {
+                        print("error")
+                    }
+                }
             }
-          }
+            task.resume()
         }
-      }
-      task.resume()
     }
-  }
 }
