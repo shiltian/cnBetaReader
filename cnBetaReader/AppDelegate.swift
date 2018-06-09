dd67678156e5a33c860e06f3c820e7197b232a45
Shilei Tian <tianshilei1992@gmail.com>
2018-06-09 12:01:48 +0800

    Fixed some warnings.

--- cnBetaReader/AppDelegate.swift
+++ cnBetaReader/AppDelegate.swift
@@ -60,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          error conditions that could cause the creation of the store to fail.
          */
         let container = NSPersistentContainer(name: "ArticleCoreData")
-        print(container.persistentStoreDescriptions.first?.url)
+        debugPrint(container.persistentStoreDescriptions.first?.url)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
